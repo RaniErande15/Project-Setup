79bb64f3fa74c48bc911aedbede4db6a6aa2d868
ranierande15 <ranierande15@gmail.com>
2022-01-06 09:36:43 +0530

    Project Setup

---
+++ src/basic/EvenOdd.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number=45;
+		if(number%2==0) {
+			System.out.println("Number is Even");
+		}
+		else {
+			System.out.println("Number is odd");
+		}
+		
+	}
+
+}
---
+++ src/basic/FirstProgram.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="Rani Erande";
+		System.out.println(s1);
+		
+		
+
+	}
+
+}
